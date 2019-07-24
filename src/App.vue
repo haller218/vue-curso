f0438f447f0b54ae37954f4690c1468e495102bd
haller218 <eliseu42lucena@gmail.com>
2019-07-24 15:33:38 -0300

    create components and children of then

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="corpo">
+    
+    
+    <h2 class="centralizado">{{ titulo }}</h2>
+
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotos">
+    
+        <meu-painel :titulo="foto.titulo">
+          <img class="image-responsiva":src="foto.url" :alt="foto.titulo">
+        </meu-painel>
+           
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data(){
+
+    return {
+      titulo: 'Alurapic',
+      fotos: []
+    }
+  },
+
+  created() {
+
+    this.$http.get('http://127.0.0.1:3000/v1/fotos')
+    .then( r => {
+
+      r.json().then( rs => {
+
+        this.fotos = rs
+      }, errs => console.log(errs))
+    }, err => {
+      
+      console.log("ERRO")
+      console.log ( err ) 
+    });
+  }
+}
+</script>
+
+<style>
+.corpo {
+  font-family: Helvetica,Arial, sans-serif;
+  width: 96%;
+  margin: 0 auto;
+}
+
+.centralizado {
+  text-align: center
+}
+
+.lista-fotos {
+  list-style: none;
+}
+
+.lista-fotos .lista-fotos-item {
+  display: inline;
+}
+
+.image-responsiva {
+
+  width: 100%;
+}
+</style>
